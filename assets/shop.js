d1fefc261cc757156d3e6de4dc81b7bad2f61ae6
raghavm <raghav.m2014@gmail.com>
2019-03-17 14:53:07 -0700

    Add files via upload

---
+++ assets/shop.js
@@ -0,0 +1,43 @@
+var container = document.getElementById("shop-item-container");
+
+var itemLimits = {
+	
+}
+
+var shopCode = {
+  first: "<div id='",
+	// Item Codename
+	second: "' class=\"shopcard-inline\"><h4 class=\"ttl\">",
+	// Item Title
+	third: "</h4><hr class=\"blu thin\"><p class=\"desc\">",
+	// Description Line 1
+	fourth: "<br>",
+	// Description Line 2
+	fifth: "<br>",
+	// Description Line 3
+	sixth: "<br>",
+	// Description Line 4
+	seventh: "</p><button class=\"buy-now\" onclick=\"buyShop(\'",
+	// Item Codename
+	eighth: "', ",
+	// Price
+	nineth: ")\">Buy Now! ($",
+	// ** price.toString() **
+	tenth: ")</button></div>"
+}
+
+function createShopItem(name, title, desc1, desc2, desc3, desc4, price){
+	var num1 = shopCode.first + name + shopCode.second + title + shopCode.third;
+	var num2 = desc1 + shopCode.fourth + desc2 + shopCode.fifth + desc3;
+	var num3 = shopCode.sixth + desc4 + shopCode.seventh + name + shopCode.eighth;
+	var num4 = price.toString() + shopCode.nineth + price.toString() + shopCode.tenth;
+	var div = num1 + num2 + num3 + num4;
+	console.log(div);
+	container.innerHTML = container.innerHTML + div;
+}
+
+function deleteShopItem(id){
+	document.getElementById(id).outerHTML = "";
+}
+
+createShopItem("holoflip", "HoloFlipperTM", "Jeph Besoz has made", "an amazing invention", "that lets you do", "super-cool things", 666)
