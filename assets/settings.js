a660b5c401e3b981c9ff376cc3d7d87376184375
Lehuy <44150458+LehuyH@users.noreply.github.com>
2019-03-19 21:38:55 -0700

    Remove old properties

--- assets/settings.js
+++ assets/settings.js
@@ -41,9 +41,9 @@ var ecoflipper = {
   amount: 0,
 	unlock: 2,
   wakeup: 'manageecoflipper',
-  name: 'person',
-  code:' <img class=" itemimg personflip infinite" src="assets/img/person.png">',
-	cardcode: "<div style='opacity:1'class='card'> <p>Eco Flipper</p> <div class='bar' id='ecoflipperbar'></div> <hr class='blu thin'> <div class='container' id='ecoflipperinfo'> <p>Use the wind to flip coins, then charge more because it is environmentally friendly<br>Flip Time: 1 Second<br> Money Per Flip: $0.10</p> </div> <br> <button id='buy-ecoflipper' onclick='buy(\"ecoflipper\")' class='buy-bot'>Build one for $12.00</button> <p id='ecoflipper-displaymax'>You are using 0/5</p> </div>"
+  name: 'ecoflipper',
+  code:' <img class=" itemimg spin infinite" src="assets/img/ecoflipper.png">',
+	cardcode: "<div style='opacity:1'class='card'> <p>Eco Flipper</p> <div class='bar' id='ecobar'></div> <hr class='blu thin'> <div class='container' id='ecoflipperinfo'> <p>Use the wind to flip coins, then charge more because it is environmentally friendly.<br>Flip Time: 1 Second<br> Money Per Flip: $0.10</p> </div> <br> <button id='buy-ecoflipper' onclick='buy(\"ecoflipper\")' class='martophack buy-bot'>Build one for $12.00</button> <p id='ecoflipper-displaymax'>You are using 0/5</p> </div>"
 
 } 
 
