d292c974b39447a529523caadfa06c0ae3fbf182
LehuyH <lehuy.hoang@outlook.com>
2019-03-28 18:03:07 -0700

    Add marketing items

--- assets/marketing.js
+++ assets/marketing.js
@@ -2,14 +2,7 @@ var normalContainer = document.getElementById("normalPromoContainer");
 var specialContainer = document.getElementById("specialPromoContainer");
 var boostDisplay = document.getElementById('boostDisplay')
 var boostDisplayp = document.getElementById('boostDisplayp')
-
-var contractLimits = {
-	// All can be bought infinitely.
-	"press-release": 0,
-	"ed-promo": 0,
-	"tv-promo": 0,
-	"local-support": 0
-}
+var timeout = true;
 
 function notifyMarketing(header, description, flip = true){
   noti.style.right = 'calc(100% + 300px)'
@@ -26,8 +19,8 @@ function buyCampaign(id, price, boostValue,btn){
   id = id.trim();
   var priceHund = price * 100
 	if (player.money < priceHund) {
-		notify("R&D Labs", "You can't afford that.")
-		idx = document.getElementById(id);
+		notify("Marketing", "You can't afford that.")
+		var idx = document.getElementById(id);
 		idx.style.animation = "skew-no-shop 1s";
 		setTimeout(function () {
 			idx.style.animation = "none";
@@ -49,27 +42,44 @@ function loadCampaigns(){
 	
 }
 
-function clickBooster(boost, cardId, btn, durationMS = 15000){
+function clickBooster(boost, cardId, btn, durationMS = 12000){
+  if(timeout){
+    timeout = false
 	var id = cardId
 	var card = document.getElementById(cardId);
-	card.style.backgroundColor = "rgba(255,255,255,0.5)"
+	card.style.backgroundColor = "rgb(58, 193, 98)"
+	card.style.color = "white";
+	card.querySelector("hr").classList.remove("blu");
 	btn.disabled = true 
 	btn.style.opacity = 0 
   player.clickboost = player.clickboost + boost;
-	boostDisplayp.innerText = 'Marketing Boost: + $' + player.clickboost / 10
-boostDisplay.style.opacity = 1
-	setTimeout(function(){
+	boostDisplayp.innerText = 'Marketing Boost: +' + player.clickboost * 100  + "¢";
+	boostDisplay.style.opacity = 1
+  removeBoost(durationMS, card, btn, boost)
+  setTimeout(function(){timeout = true}, 5000)
+  }else{
+    closenotify()
+ notify('Timeout', 'Please wait a couple seconds before launching a new campaign')
+  }
+}
+
+function removeBoost(time, card, btn, boost){
+setTimeout(function(){
 		notify("Campaign Completed", "A marketing campaign has ended.");
 		card.style.backgroundColor = "rgba(255,255,255,1)"
 		btn.style.opacity = 1
-		btn.disabled = false 
-		player.clickboost = player.clickboost -	 boost;
-		boostDisplayp.innerText = 'Marketing Boost: + $' + player.clickboost / 10
-		if(player.clickboost <= 0){
+		btn.disabled = false;
+		card.querySelector("hr").classList.add("blu");
+		card.style.color = "black";
+		card.style.backgroundColor = "white"
+    player.clickboost -=  Math.round(boost * 100) / 100
+		boostDisplayp.innerText = 'Marketing Boost: +' + player.clickboost * 100  + "¢";
+		if(player.clickboost < 0 || player.clickboost == 0){
 			boostDisplay.style.opacity = 0
 		}
-	}, durationMS);
-}
+	}, time);
 
+
+}
 
 
